cbaec3e3fa54b7c851b111886d20b6c2dd83147f
imaciej10 <irlik.maciej95@gmail.com>
2024-05-14 08:19:41 +0200

    add ESlint rules, webpack set up and basic html template&stylesheet

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+    main: "./src/main.js",
+  },
+  devtool: "inline-source-map",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: "",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack 5",
+      filename: `index.html`,
+      template: "src/index.html",
+      inject: "body",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
