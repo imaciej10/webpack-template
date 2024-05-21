ef4e5d8c929cee7c73cac6c63a83633f9a2378b6
imaciej10 <imaciej10@users.noreply.github.com>
2024-05-21 18:12:17 +0200

    Add babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./style.css";
+
+const openNavBtn = document.querySelector(".openNav");
+const menuButtons = document.querySelectorAll(".menuBtn");
+
+function activateDropdown(menuBttns, openBtn) {
+  openBtn.addEventListener("click", () => {
+    menuBttns.forEach((button) => button.classList.toggle("active"));
+    openBtn.classList.toggle("toggled");
+  });
+}
+
+activateDropdown(menuButtons, openNavBtn);
